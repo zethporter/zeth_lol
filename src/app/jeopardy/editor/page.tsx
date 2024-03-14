0398f12a891802acf2660d1e4932a170f605d440
zeth <zeth@pop-os.localdomain>
2024-03-13 22:26:44 -0600

    added ability to upload saved games to the editor

--- src/app/jeopardy/editor/page.tsx
+++ src/app/jeopardy/editor/page.tsx
@@ -1,9 +1,13 @@
+"use-client";
+
 import EditorForm from "./EditorForm";
+import { Toaster } from "react-hot-toast";
 
 export default function Editor() {
   return (
     <main>
       <EditorForm />
+      <Toaster />
     </main>
   );
 }
--- src/app/jeopardy/page.tsx
+++ src/app/jeopardy/page.tsx
@@ -1,4 +1,6 @@
 "use client";
+// import { Toaster } from "react-hot-toast";
+
 import QuestionModal from "./QuestionModal";
 import JeopardyHeader from "./JeopardyHeader";
 import GameBoard from "./GameBoard";
@@ -11,7 +13,7 @@ export default function Jeopardy() {
       <GameBoard>
         <div className="flex h-full w-full flex-col-reverse gap-2 bg-base-300/75 p-2">
           <div className="container mx-auto grid flex-1 grid-cols-6 gap-2 rounded-xl p-2">
-            {newGame.topics.map((topic, i) => (
+            {newGame.game.topics.map((topic, i) => (
               <div key={i} className="grid grid-rows-6 gap-2">
                 <div className="flex w-full items-center justify-center rounded-lg bg-accent font-sans text-2xl font-bold text-accent-content">
                   {topic.topicLabel}
