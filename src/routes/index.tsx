885003e7bbdf14693989defe35ed6a18c0441933
zeth porter <zethwporter@gmail.com>
2026-01-25 20:07:02 -0700

    set up empty db for auth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,26 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 import { AuroraText } from "@/components/ui/aurora-text";
 import { FlickeringGrid } from "@/components/ui/flickering-grid";
+import { Button } from "@/components/ui/button";
+import { ChevronRight } from "lucide-react";
 
 export const Route = createFileRoute("/")({ component: App });
 
 function App() {
   return (
     <div className="relative h-screen w-full">
-      <div className=" absolute left-[50%] top-[50%] -translate-y-[50%] -translate-x-[50%] z-20">
+      <div className="flex flex-col gap-3 items-center absolute left-[50%] top-[50%] -translate-y-[50%] -translate-x-[50%] z-20">
         <AuroraText
           colors={["#022c22", "#047857", "#34d399", "#6ee7b7"]}
           className="text-6xl font-bold"
         >
           Zeth.lol
         </AuroraText>
+        <Link to="/">
+          <Button variant="default" size="lg">
+            Jeopardy <ChevronRight />
+          </Button>
+        </Link>
       </div>
 
       <FlickeringGrid
