8cb6c3284ec457ff571789a362338ea64453928b
zeth porter <zethwporter@gmail.com>
2026-02-26 21:54:09 -0700

    started actually writing up server functions for managing jeopardy shiz

--- src/lib/data-management/zeo.ts
+++ src/lib/data-management/zeo.ts
@@ -27,6 +27,7 @@ import {
   zeoSharedInsertSchema,
 } from "@/db/schema/app";
 import { db } from "@/db/app";
+import z from "zod";
 
 export const getGamesList = createServerFn({ method: "GET" }).handler(
   async () => {
@@ -41,4 +42,52 @@ export const insertNewGame = createServerFn({ method: "POST" })
   .handler(async ({ data }) => {
     const game = await db.insert(zeoGames).values(data).returning();
     return game;
+  });
+
+export const getCategories = createServerFn({ method: "GET" }).handler(
+  async () => {
+    const categories = await db.select().from(zeoCategories);
+    return categories;
+  },
+);
+
+export const insertCategory = createServerFn({ method: "POST" })
+  .inputValidator(zeoCategoriesInsertSchema.parse)
+  .handler(async ({ data }) => {
+    const category = await db.insert(zeoCategories).values(data).returning();
+    return category;
+  });
+
+export const insertRound = createServerFn({ method: "POST" })
+  .inputValidator(zeoRoundsInsertSchema.parse)
+  .handler(async ({ data }) => {
+    const round = await db.insert(zeoRounds).values(data).returning();
+    return round;
+  });
+
+export const updateRound = createServerFn({ method: "POST" })
+  .inputValidator(zeoRoundsUpdateSchema.parse)
+  .handler(async ({ data }) => {
+    if (data.id) {
+      const updatedRound = await db
+        .update(zeoRounds)
+        .set(data)
+        .where(eq(zeoRounds.id, data.id))
+        .returning();
+      return updatedRound;
+    }
+    throw new Error("Invalid round ID");
+  });
+
+const GetGameRoundsSchema = z.object({
+  gameId: z.uuid(),
+});
+export const getGameRounds = createServerFn({ method: "GET" })
+  .inputValidator(GetGameRoundsSchema.parse)
+  .handler(async ({ data }) => {
+    const rounds = await db
+      .select()
+      .from(zeoRounds)
+      .where(eq(zeoRounds.gameId, data.gameId));
+    return rounds;
   });
